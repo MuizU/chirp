fe7f4979140bb75eb06ba686019d9fa48d4ceac9
muizu <muizuvais99@gmail.com>
2023-04-14 18:01:46 +0530

    Fixed type issues

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,8 @@
-import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }): any => {
+    getAll: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.post.findMany();
     }),
 });
