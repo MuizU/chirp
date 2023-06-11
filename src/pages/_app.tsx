db5cea0e7ed34a3595d1e5ec3ef5875dfd61e8ff
muizu <muizuvais99@gmail.com>
2023-06-11 17:05:18 +0530

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
-import {type AppProps, type AppType} from "next/app";
-
+import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
-  return (
-      <ClerkProvider {...pageProps} >
-      <Component {...pageProps} />
-      </ClerkProvider>
-  );
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
 
 export default api.withTRPC(MyApp);
